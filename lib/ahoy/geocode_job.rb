7e420e2fd21dae591a331ced541bdf0707fc505e
Shiv Bharthur <shiv@recroup.com>
2015-09-16 13:20:21 -0400

    encoding fix

--- lib/ahoy/geocode_job.rb
+++ lib/ahoy/geocode_job.rb
@@ -5,12 +5,13 @@ module Ahoy
     def perform(visit)
       deckhand = Deckhands::LocationDeckhand.new(visit.ip)
       Ahoy::VisitProperties::LOCATION_KEYS.each do |key|
-        valu = deckhand.send(key)
-        valu = valu.encode('UTF-8') if valu.class.to_s == "String"
-        visit.send(:"#{key}=", valu) if visit.respond_to?(:"#{key}=")
-      end
-      visit.save!
+        if visit.respond_to?(:"#{key}=")
+          valu = deckhand.send(key)
+          valu = valu.encode('UTF-8') if valu.class.to_s == "String"
+          visit.send(:"#{key}=", valu)
+        end
+      end; visit.save!
     end
   end
-  
+
 end
